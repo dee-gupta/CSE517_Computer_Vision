2aac75e01371b2bb1babb9ca8a3674749f04e4e6
Deepak Gupta <deegupta@cs.stonybrook.edu>
2017-11-12 15:07:52 -0500

    Panorama stitching, Tracking, and Segmentation.

---
+++ DetectionTracking/ipython/itrack.py
@@ -0,0 +1,341 @@
+import os
+import sys
+import cv2
+import numpy as np
+
+#face_cascade = cv2.CascadeClassifier('/usr/local/share/OpenCV/#haarcascadeshaarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def help_message():
+   print("Usage: [Question_Number] [Input_Video] [Output_Directory]")
+   print("[Question Number]")
+   print("1 Camshift")
+   print("2 Particle Filter")
+   print("3 Kalman Filter")
+   print("4 Optical Flow")
+   print("[Input_Video]")
+   print("Path to the input video")
+   print("[Output_Directory]")
+   print("Output directory")
+   print("Example usages:")
+   print(sys.argv[0] + " 1 " + "02-1.avi " + "./")
+
+def detect_one_face(im):
+    gray=cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.2, 3)
+    if len(faces) == 0:
+        return (0,0,0,0)
+    return faces[0]
+
+def hsv_histogram_for_window(frame, window):
+    # set up the ROI for tracking
+    c,r,w,h = window
+    roi = frame[r:r+h, c:c+w]
+    hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
+    return roi_hist
+
+
+def resample(weights):
+    n = len(weights)
+    indices = []
+    C = [0.] + [sum(weights[:i+1]) for i in range(n)]
+    u0, j = np.random.random(), 0
+    for u in [(u0+i)/n for i in range(n)]:
+      while u > C[j]:
+          j+=1
+      indices.append(j-1)
+    return indices
+
+
+def mycamshift(v, file_name):
+    
+    # Open output file
+    output_name = sys.argv[3] + file_name
+    output = open(output_name,"w")    
+    
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    outvid = cv2.VideoWriter(output_name+'.mp4', fourcc, 20.0, (int(v.get(3)),int(v.get(4))))
+    
+    frameCounter = 0
+    
+    # read first frame
+    ret ,frame = v.read()
+    if ret == False:
+        return
+
+    # detect face in first frame
+    c,r,w,h = detect_one_face(frame)
+
+    # Write track point for first frame
+    pt_x, pt_y = c + w / 2, r + h / 2
+    output.write("%d,%d,%d\n" % (0, pt_x, pt_y)) # Write as 0,pt_x,pt_y
+    
+    frameCounter = frameCounter + 1
+
+    # set the initial tracking window
+    track_window = (c, r, w, h)
+
+    # calculate the HSV histogram in the window
+    roi_hist = hsv_histogram_for_window(frame, (c, r, w, h)) # this is provided for you
+
+    term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
+
+    while(True):
+        ret, frame = v.read() # read another frame
+        if ret == False:
+            break
+
+        #change colorspace to hsv
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        #calculating backprojection
+        dst = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+
+        #meanshift to get new location
+        ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+
+        #drawing it on image
+        pts = cv2.boxPoints(ret)
+	
+        pt_x = (pts[0][0] + pts[2][0]) / 2
+        pt_y = (pts[0][1] + pts[2][1]) / 2
+
+        cv2.circle(frame,(int(pt_x),int(pt_y)), 2, (0,0,255), -1)
+        outvid.write(frame)
+
+        # write the result to the output file
+        output.write("%d,%d,%d\n" % (frameCounter, pt_x, pt_y)) # Write as frame_index,pt_x,pt_y
+        frameCounter = frameCounter + 1
+
+    output.close()
+    outvid.release()
+
+
+def myparticle(v, file_name):
+    # Open output file
+    output_name = sys.argv[3] + file_name
+    output = open(output_name,"w")
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    outvid = cv2.VideoWriter(output_name+'.mp4', fourcc, 20.0, (int(v.get(3)),int(v.get(4))))
+
+    frameCounter = 0
+    # read first frame
+    ret ,frame = v.read()
+    if ret == False:
+        return
+
+    # detect face in first frame
+    c,r,w,h = detect_one_face(frame)
+
+    # Write track point for first frame
+    pt_x, pt_y = c + w / 2, r + h / 2
+    output.write("%d,%d,%d\n" % (0, pt_x, pt_y)) # Write as 0,pt_x,pt_y
+    frameCounter = frameCounter + 1
+
+    def particleevaluator(back_proj, particle):
+        return back_proj[particle[1], particle[0]]
+
+    # hist_bp: obtain using cv2.calcBackProject and the HSV histogram
+    # c,r,w,h: obtain using detect_one_face()
+    n_particles = 300
+    roi_hist = hsv_histogram_for_window(frame, (c,r,w,h)) # this is provided for you
+
+    #change colorspace to hsv
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #calculate back projection
+    hist_bp = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+
+    init_pos = np.array([c + w / 2.0, r + h / 2.0], int) # initial position to center of detected face
+    particles = np.ones((n_particles, 2), int) * init_pos # init particles to center position
+    f0 = particleevaluator(hist_bp, init_pos) * np.ones(n_particles) # Evaluate appearance model
+    weights = np.ones(n_particles) / n_particles   # weights are uniform (at first)
+
+    while(True):
+        ret ,frame = v.read() # read another frame
+        if ret == False:
+            break
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hist_bp = cv2.calcBackProject([hsv], [0], roi_hist, [0, 180], 1)
+        stepsize = 20
+
+        # Particle motion model: uniform step (TODO: find a better motion model)
+        np.add(particles, np.random.uniform(-stepsize, stepsize, particles.shape), out=particles, casting="unsafe")
+
+        # Clip out-of-bounds particles
+        particles = particles.clip(np.zeros(2), np.array((frame.shape[1], frame.shape[0])) - 1).astype(int)
+
+        f = particleevaluator(hist_bp, particles.T) # Evaluate particles
+        weights = np.float32(f.clip(1))             # Weight ~ histogram response
+        weights /= np.sum(weights)                  # Normalize w
+        pos = np.sum(particles.T * weights, axis=1).astype(int) # expected position: weighted average
+
+        if 1. / np.sum(weights**2) < n_particles / 2.: # If particle cloud degenerate:
+            particles = particles[resample(weights),:]  # Resample particles according to weights
+
+        for xy in particles:
+            cv2.circle(frame,(xy[0],xy[1]), 1, (0,0,255), -1)
+        
+        outvid.write(frame)
+        # write the result to the output file
+        output.write("%d,%d,%d\n" % (frameCounter, pos[0], pos[1])) # Write as frame_index,pt_x,pt_y
+        frameCounter = frameCounter + 1
+    
+    outvid.release()
+    output.close()
+
+
+def mykalman(v, file_name):
+    # Open output file
+    output_name = sys.argv[3] + file_name
+    output = open(output_name,"w")
+
+    frameCounter = 0
+    # read first frame
+    ret ,frame = v.read()
+    if ret == False:
+        return
+
+    # detect face in first frame
+    c,r,w,h = detect_one_face(frame)
+
+    # Write track point for first frame
+    pt_x, pt_y = c + w / 2, r + h / 2
+    output.write("%d,%d,%d\n" % (0, pt_x, pt_y)) # Write as 0,pt_x,pt_y
+    frameCounter = frameCounter + 1
+
+    # initialize the tracker
+    state = np.array([c + w / 2,r + h / 2, 0 ,0], dtype='float64') # initial position
+    kalman = cv2.KalmanFilter(4,2,0) # 4 state/hidden, 2 measurement, 0 control
+    kalman.transitionMatrix = np.array([[1., 0., .1, 0.],  # a rudimentary constant speed model:
+                                        [0., 1., 0., .1],  # x_t+1 = x_t + v_t
+                                        [0., 0., 1., 0.],
+                                        [0., 0., 0., 1.]])
+    kalman.measurementMatrix = 1 * np.eye(2, 4)      # you can tweak these to make the tracker
+    kalman.processNoiseCov = 1e-2 * np.eye(4, 4)      # respond faster to change and be less smooth
+    kalman.measurementNoiseCov = 1e-2 * np.eye(2, 2)
+    kalman.errorCovPost = 1 * np.eye(4, 4)
+    kalman.statePost = state
+
+    while(1):
+        ret ,frame = v.read() # read another frame
+        if ret == False:
+            break
+
+        c, r, w, h = detect_one_face(frame)
+        measurement_valid = not (c == 0 and r == 0 and w == 0 and h == 0)
+
+        prediction = kalman.predict()
+
+        pt_x, pt_y = prediction[0][0], prediction[1][0]
+
+        if measurement_valid: # e.g. face found
+            measurement = np.array([c + w / 2, r + h / 2], dtype='float64')
+            posterior = kalman.correct(measurement)
+            pt_x, pt_y = posterior[0], posterior[1]
+
+        # write the result to the output file
+        output.write("%d,%d,%d\n" % (frameCounter, pt_x, pt_y)) # Write as frame_index,pt_x,pt_y
+        frameCounter = frameCounter + 1
+
+    output.close()
+
+
+def myof(v, file_name):
+    # Open output file
+    output_name = sys.argv[3] + file_name
+    output = open(output_name,"w")
+
+    frameCounter = 0
+    # read first frame
+    ret ,frame = v.read()
+    if ret == False:
+        return
+
+    # detect face in first frame
+    c,r,w,h = detect_one_face(frame)
+
+    # Write track point for first frame
+    pt_x, pt_y = c + w / 2, r + h / 2
+    output.write("%d,%d,%d\n" % (0, pt_x, pt_y)) # Write as 0,pt_x,pt_y
+    frameCounter = frameCounter + 1
+
+    # params for ShiTomasi corner detection
+    feature_params = dict( maxCorners = 10,
+                           qualityLevel = 0.3,
+                           minDistance = 1,
+                           blockSize = 6 )
+
+    # Parameters for lucas kanade optical flow
+    lk_params = dict( winSize  = (50, 50),
+                      maxLevel = 2,
+                      criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1))
+
+    prev_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    mask = np.zeros((prev_frame.shape[0], prev_frame.shape[1]), np.uint8)
+    mask[pt_y - 30 : pt_y + 20, pt_x - 30 : pt_x + 20] = 255
+
+    prev_pts = cv2.goodFeaturesToTrack(prev_frame, mask = mask, **feature_params)
+    # prev_pts = np.array([[[pt_x, pt_y]]], dtype='float32')
+
+    while(1):
+        ret ,frame = v.read() # read another frame
+        if ret == False:
+            break
+
+        # track face
+        c, r, w, h = detect_one_face(frame)
+        measurement_valid = not (c == 0 and r == 0 and w == 0 and h == 0)
+
+        next_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        next_pts, status, err = cv2.calcOpticalFlowPyrLK(prev_frame, next_frame, prev_pts, None, **lk_params)
+        next_pts = next_pts[status == 1]
+
+        pt_x, pt_y = c + w / 2, r + h / 2
+
+        # getting avg of point if measurement isn't valid
+        if not measurement_valid:
+            x, y = 0, 0
+            for pt in next_pts:
+                x += pt[0]
+                y += pt[1]
+            pt_x, pt_y = int(x) / len(next_pts), int(y) / len(next_pts)
+
+        output.write("%d,%d,%d\n" % (frameCounter, pt_x, pt_y)) # Write as frame_index,pt_x,pt_y
+        frameCounter = frameCounter + 1
+        prev_frame = next_frame.copy()
+        prev_pts = next_pts.reshape(-1, 1, 2)
+
+    output.close()
+
+
+if __name__ == '__main__':
+    question_number = -1
+   
+    # Validate the input arguments
+    if (len(sys.argv) != 4):
+        help_message()
+        sys.exit()
+    else: 
+        question_number = int(sys.argv[1])
+        if (question_number > 4 or question_number < 1):
+            print("Input parameters out of bound ...")
+            sys.exit()
+
+    # read video file
+    video = cv2.VideoCapture(sys.argv[2]);
+
+    if (question_number == 1):
+        mycamshift(video, "output_camshift.txt")
+    elif (question_number == 2):
+        myparticle(video, "output_particle.txt")
+    elif (question_number == 3):
+        mykalman(video, "output_kalman.txt")
+    elif (question_number == 4):
+        myof(video, "output_of.txt")
---
+++ Segmentation/Deepak_Gupta_111485745/Source/main_bonus.py
@@ -0,0 +1,206 @@
+# ================================================
+# Skeleton codes for HW4
+# Read the skeleton codes carefully and put all your
+# codes into main function
+# ================================================
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
+from skimage.data import astronaut
+from skimage.util import img_as_float
+import maxflow
+from scipy.spatial import Delaunay
+import sys
+
+def help_message():
+   print("Usage: [Input_Image] [Input_Marking] [Output_Directory]")
+   print("[Input_Image]")
+   print("Path to the input image")
+   print("[Input_Marking]")
+   print("Path to the input marking")
+   print("[Output_Directory]")
+   print("Output directory")
+   print("Example usages:")
+   print(sys.argv[0] + " astronaut.png " + "astronaut_marking.png " + "./")
+
+# Calculate the SLIC superpixels, their histograms and neighbors
+def superpixels_histograms_neighbors(img):
+    # SLIC
+    segments = slic(img, n_segments=500, compactness=20)
+    segments_ids = np.unique(segments)
+
+    # centers
+    centers = np.array([np.mean(np.nonzero(segments==i),axis=1) for i in segments_ids])
+
+    # H-S histograms for all superpixels
+    hsv = cv2.cvtColor(img.astype('float32'), cv2.COLOR_BGR2HSV)
+    bins = [20, 20] # H = S = 20
+    ranges = [0, 360, 0, 1] # H: [0, 360], S: [0, 1]
+    colors_hists = np.float32([cv2.calcHist([hsv],[0, 1], np.uint8(segments==i), bins, ranges).flatten() for i in segments_ids])
+
+    # neighbors via Delaunay tesselation
+    tri = Delaunay(centers)
+
+    return (centers,colors_hists,segments,tri.vertex_neighbor_vertices)
+
+# Get superpixels IDs for FG and BG from marking
+def find_superpixels_under_marking(marking, superpixels):
+    fg_segments = np.unique(superpixels[marking[:,:,0]!=255])
+    bg_segments = np.unique(superpixels[marking[:,:,2]!=255])
+    return (fg_segments, bg_segments)
+
+# Sum up the histograms for a given selection of superpixel IDs, normalize
+def cumulative_histogram_for_superpixels(ids, histograms):
+    h = np.sum(histograms[ids],axis=0)
+    return h / h.sum()
+
+# Get a bool mask of the pixels for a given selection of superpixel IDs
+def pixels_for_segment_selection(superpixels_labels, selection):
+    pixels_mask = np.where(np.isin(superpixels_labels, selection), True, False)
+    return pixels_mask
+
+# Get a normalized version of the given histograms (divide by sum)
+def normalize_histograms(histograms):
+    return np.float32([h / h.sum() for h in histograms])
+
+# Perform graph cut using superpixels histograms
+def do_graph_cut(fgbg_hists, fgbg_superpixels, norm_hists, neighbors):
+    num_nodes = norm_hists.shape[0]
+    # Create a graph of N nodes, and estimate of 5 edges per node
+    g = maxflow.Graph[float](num_nodes, num_nodes * 5)
+    # Add N nodes
+    nodes = g.add_nodes(num_nodes)
+
+    hist_comp_alg = cv2.HISTCMP_KL_DIV
+
+    # Smoothness term: cost between neighbors
+    indptr,indices = neighbors
+    for i in range(len(indptr)-1):
+        N = indices[indptr[i]:indptr[i+1]] # list of neighbor superpixels
+        hi = norm_hists[i]                 # histogram for center
+        for n in N:
+            if (n < 0) or (n > num_nodes):
+                continue
+            # Create two edges (forwards and backwards) with capacities based on
+            # histogram matching
+            hn = norm_hists[n]             # histogram for neighbor
+            g.add_edge(nodes[i], nodes[n], 20-cv2.compareHist(hi, hn, hist_comp_alg),
+                                           20-cv2.compareHist(hn, hi, hist_comp_alg))
+
+    # Match term: cost to FG/BG
+    for i,h in enumerate(norm_hists):
+        if i in fgbg_superpixels[0]:
+            g.add_tedge(nodes[i], 0, 1000) # FG - set high cost to BG
+        elif i in fgbg_superpixels[1]:
+            g.add_tedge(nodes[i], 1000, 0) # BG - set high cost to FG
+        else:
+            g.add_tedge(nodes[i], cv2.compareHist(fgbg_hists[0], h, hist_comp_alg),
+                                  cv2.compareHist(fgbg_hists[1], h, hist_comp_alg))
+
+    g.maxflow()
+    return g.get_grid_segments(nodes)
+
+def RMSD(target, master):
+    # Note: use grayscale images only
+
+    # Get width, height, and number of channels of the master image
+    master_height, master_width = master.shape[:2]
+    master_channel = len(master.shape)
+
+    # Get width, height, and number of channels of the target image
+    target_height, target_width = target.shape[:2]
+    target_channel = len(target.shape)
+
+    # Validate the height, width and channels of the input image
+    if (master_height != target_height or master_width != target_width or master_channel != target_channel):
+        return -1
+    else:
+
+        total_diff = 0.0;
+        dst = cv2.absdiff(master, target)
+        dst = cv2.pow(dst, 2)
+        mean = cv2.mean(dst)
+        total_diff = mean[0]**(1/2.0)
+
+        return total_diff;
+
+def calculate_mask_and_show():
+    fgSegment, bgSegment = find_superpixels_under_marking(whiteImg, segment1)
+    cumHistFg = cumulative_histogram_for_superpixels(fgSegment, colorHist1)
+    cumHistBg = cumulative_histogram_for_superpixels(bgSegment, colorHist1)
+    res = do_graph_cut((cumHistFg, cumHistBg), (fgSegment, bgSegment), norm_hist,
+                       neighbourVertices1)
+    mask = np.where(res[segment1], 255, 0).astype('uint8')
+    cv2.imshow("Mask", mask)
+
+def draw_line(event, x, y, flags, param):
+    global ix, iy, drawing, mode, redLine, blueLine
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+        if mode == True:
+            redLine = True
+        else:
+            blueLine = True
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            if mode == True:
+                cv2.line(whiteImg, (ix, iy), (x, y), (255, 0, 0), 4)
+                cv2.line(pixel_image, (ix, iy), (x, y), (255, 0, 0), 4)
+
+            else:
+                cv2.line(whiteImg, (ix, iy), (x, y), (0, 0, 255), 4)
+                cv2.line(pixel_image, (ix, iy), (x, y), (0, 0, 255), 4)
+
+        # cv2.imshow('canvas', whiteImg)
+        cv2.imshow('astronaut', pixel_image)
+        if blueLine is True and redLine is True:
+            calculate_mask_and_show()
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        # cv2.imshow('canvas', whiteImg)
+        cv2.imshow('astronaut', pixel_image)
+        if redLine is True and blueLine is True:
+            calculate_mask_and_show()
+
+
+if __name__ == '__main__':
+   
+    # validate the input arguments
+    if (len(sys.argv) != 2):
+        help_message()
+        sys.exit()
+
+    mainImg = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
+    whiteImg = np.ones((mainImg.shape[0], mainImg.shape[1], 3), np.uint8) * 255
+
+    center1, colorHist1, segment1, neighbourVertices1 = superpixels_histograms_neighbors(mainImg)
+    norm_hist = normalize_histograms(colorHist1)
+
+    drawing = False  # true if mouse is pressed
+    mode = True  # if True, draw rectangle. Press 'm' to toggle to curve
+    ix, iy = -1, -1
+    redline = False
+    blueLine = False
+    
+    pixel_image = mainImg
+    cv2.namedWindow('astronaut')
+    cv2.setMouseCallback('astronaut', draw_line)
+    cv2.imshow('astronaut', pixel_image)
+    
+    while 1:
+        k = cv2.waitKey(1) & 0xFF
+        if k == ord('m'):
+            mode = not mode
+        elif k == 27:
+            break
+
+    cv2.destroyAllWindows()
+
